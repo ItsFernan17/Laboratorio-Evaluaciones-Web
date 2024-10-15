2a7a202430ddaaf67f0e9d7864c9539b83f441bd
Fernando Rivas <frivasm@miumg.edu.gt>
2024-10-14 19:13:04 -0600

    Importanto Tipografia INTER

--- astro-frontend/src/components/Poblacion.jsx
+++ astro-frontend/src/components/Poblacion.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { useFetch } from '../useFetch'
+import React from "react";
+import { useFetch } from "../useFetch";
 
-function Poblacion() {
-  const { data } = useFetch('http://localhost:3000/api/v1/poblacion')
+function Poblacion({ register }) {
+  const { data } = useFetch("http://localhost:3000/api/v1/poblacion");
   return (
-    <select className="bg-[#F7FAFF] h-[34px] mb-4 w-[320px] mt-1 rounded-sm shadow-sm border border-primary pl-3 font-page">
+    <select
+      {...register("codigo_poblacion")}
+      className="bg-[#F7FAFF] h-[34px] mb-4 w-[320px] mt-1 rounded-sm shadow-sm border border-primary pl-3 font-page"
+    >
       <option value="0">Seleccione una Poblacion</option>
       {data?.map((poblacion, index) => (
-        <option key={index} value={poblacion.nombre_poblacion}>
+        <option key={index} value={index + 1}>
           {poblacion.nombre_poblacion}
         </option>
       ))}
     </select>
-  )
+  );
 }
 
-export default Poblacion
+export default Poblacion;
