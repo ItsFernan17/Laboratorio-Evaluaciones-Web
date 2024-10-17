f7ed6459acbaf1d485872415162bc07ff5e837fc
Erick Alcon <ealconv@miumg.edu.gt>
2024-10-16 22:19:28 -0600

    Empleos frontend

--- astro-frontend/src/components/Empleos/Empleo.api.ts
+++ astro-frontend/src/components/Empleos/Empleo.api.ts
@@ -1,5 +1,9 @@
-export async function createEmpleo(newEmpleo: any){
+export async function getEmpleoCEOM(ceom: string){
+    const response = await fetch(`http://localhost:3000/api/v1/empleo/${ceom}`);
+    return await response.json();
+}
 
+export async function createEmpleo(newEmpleo: any){
     const response = await fetch('http://localhost:3000/api/v1/empleo/', {
         method: 'POST',
         headers: {
@@ -11,4 +15,24 @@ export async function createEmpleo(newEmpleo: any){
     const data = await response.json();
 
     console.log(data);
+}
+
+export async function updateEmpleo(ceom: string, newEmpleo: any){
+    const response = await fetch(`http://localhost:3000/api/v1/empleo/${ceom}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newEmpleo)
+      }); 
+  
+      return await response.json();
+  }
+
+  export async function desactiveEmpleo(ceom: string){
+    const res = await fetch(`http://localhost:3000/api/v1/empleo/${ceom}/estado`, {
+        method: 'PATCH',
+    });
+
+    return await res.json();
 }
